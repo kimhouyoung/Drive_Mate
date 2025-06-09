2ff3f3cfc61ce86c810f32f5a7a1cc454421cc69
kimhouyoung <kim744200816@gmail.com>
2025-06-09 23:06:32 +0900

    commit

--- lib/moudleb/homeview.dart
+++ lib/moudleb/homeview.dart
@@ -34,7 +34,6 @@ class _HomeWidget extends State<HomeView> {
           await Future.delayed(Duration(milliseconds: 1000));
           setState(() {});
         },
-
         child: SingleChildScrollView(
           child: Column(
             children: [
